9855afb69e894fcbe312bed43862e9bed2501993
Antoni Pekala <antonipekala@outlook.com>
2023-11-24 15:02:39 +0100

    detekcja z printowaniem

--- Sensor/sensor/main.py
+++ Sensor/sensor/main.py
@@ -1,25 +1,29 @@
+import datetime
 import multiprocessing
 import time
 from multiprocessing import Process, Queue
 
 from microphone_controller import record_sound
 from birdnet_controller import BirdNetController
-
+from utils import *
 
 def analyze(sound_samples):
     lat, lon = 52.21885, 21.01077
     min_conf = 0.5
+
+
     analyzer = BirdNetController(lat=lat, lon=lon, min_conf=min_conf)
+
 
     while True:
         rec, sr, rec_start, rec_end = sound_samples.get()
-        print(rec_start, rec_end)
         det = analyzer.analyze(rec, sr)
+
+        print(rec_start, rec_end)
         print(det)
 
 
 if __name__ == '__main__':
-
     # start analyzer process
     sound_samples = Queue()
     analyzer = Process(target=analyze, args=(sound_samples,))
